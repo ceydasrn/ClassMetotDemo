8d8eeb4a04661b0ed263966cd5a00648308b22ed
Ceyda Saran <cluise61@outlook.com>
2021-01-20 19:43:12 +0300

    Ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public static Musteri MusteriEkle(int id, string isim, string soyisim, int yas)
+        {
+            Musteri musteri = new Musteri();
+            musteri.id = id;
+            musteri.isim = isim;
+            musteri.soyisim = soyisim;
+            musteri.yas = yas;
+
+            return musteri;
+        }
+        public static void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşterinin adı: " + musteri.isim);
+            Console.WriteLine("Müşterinin soyadı: " + musteri.soyisim);
+            Console.WriteLine("Müşterinin yaşı: " + musteri.yas);
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,17 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = MusteriManager.MusteriEkle(1, "Ceyda", "Saran", 21);
+            Musteri musteri2 = MusteriManager.MusteriEkle(2, "Baran", "Taşçı", 26);
+            Musteri musteri3 = MusteriManager.MusteriEkle(3, "Burak", "Yılmaz", 23);
+            Musteri musteri4 = MusteriManager.MusteriEkle(4, "Ali", "Şahin", 19);
+
+            MusteriManager.MusteriListele(musteri1);
+            MusteriManager.MusteriListele(musteri2);
+            MusteriManager.MusteriListele(musteri3);
+            MusteriManager.MusteriListele(musteri4);
+            
+
         }
     }
 }
